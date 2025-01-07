d1e4ea72ab6eb0036a6848407f501cd59014b909
gftytkklt <602022230063@smail.nju.edu.com>
2025-01-07 22:44:47 +0800

    add result getter interface

--- include/analyzer.h
+++ include/analyzer.h
@@ -2,6 +2,15 @@
 #define ANALYZER_H
 #include "graph.h"
 
+using DataMatrix = std::vector<std::vector<int>>;
+
+struct DeployInfo {
+    std::pair<int, int> tile_id; // location
+    std::vector<std::pair<int, int>> child_tile; // child location
+    std::vector<Path> paths; // all paths with this tile as source
+    int compute_volume; // compute volume
+};
+
 struct HWInfo {
     // (WL, BL) of tile array
     std::pair<int, int> xbar_size;
@@ -22,6 +31,12 @@ class Analyzer {
 public:
     Analyzer() = default;
     Analyzer(const std::vector<NNkernel> kernels, HWInfo info, OptInfo opt);
+    struct AnalysisResult {
+        std::vector<DeployInfo> deploy_info; // deployment info of chip
+        std::vector<DataMatrix> datas; // transfer data volume matricies
+    };
+    AnalysisResult get_analysis_result() const { return result; }
+    void generate_analysis_result();
 private:
     bool mapping_opt = true;
     bool sched_opt = true;
@@ -29,7 +44,6 @@ private:
     TGraph tg;
     HGraph hg;
     DGraph dg;
-
+    AnalysisResult result;
 };
-
 #endif
